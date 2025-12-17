3b5336df07916528da1e6649d85992e766402c49
Jaehwan Kim <me@jaehwan.kim>
2025-12-18 00:21:38 +0900

    fix: correct alias command addition logic in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,17 +5,17 @@ MARKER="### alias commands for ps tools ###"
 
 if grep -qF "$MARKER" ~/.bashrc; then
     echo "Alias commands already exist in ~/.bashrc"
-    exit 0
+else
+    echo "" >> ~/.bashrc
+    echo "### alias commands for ps tools ###" >> ~/.bashrc
+    echo "alias run=$script_dir/run.sh" >> ~/.bashrc
+    echo "alias stress=$script_dir/stress.sh" >> ~/.bashrc
+    echo "alias copy=$script_dir/copy.sh" >> ~/.bashrc
+    echo "### End of alias commands ###" >> ~/.bashrc
+
+    echo "The alias commands were added in ~/.bashrc"
 fi
 
-echo "" >> ~/.bashrc
-echo "### alias commands for ps tools ###" >> ~/.bashrc
-echo "alias run=$script_dir/run.sh" >> ~/.bashrc
-echo "alias stress=$script_dir/stress.sh" >> ~/.bashrc
-echo "alias copy=$script_dir/copy.sh" >> ~/.bashrc
-echo "### End of alias commands ###" >> ~/.bashrc
-
-echo "The alias commands were added in ~/.bashrc"
 
 mkdir -p "$script_dir/src"
 touch "$script_dir/src/input.txt"
